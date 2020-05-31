419ecf10d5bb5752f8eb8366d946d2df95b45377
Sufiandy Elmy <elmysufiandy09@gmail.com>
2020-05-31 15:05:08 +0700

    Initial Commit

---
+++ Food with coreml and coredata/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Food with coreml and coredata
+//
+//  Created by Sufiandy Elmy on 31/05/20.
+//  Copyright © 2020 Sufiandy Elmy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Food with coreml and coredataTests/Food_with_coreml_and_coredataTests.swift
@@ -0,0 +1,34 @@
+//
+//  Food_with_coreml_and_coredataTests.swift
+//  Food with coreml and coredataTests
+//
+//  Created by Sufiandy Elmy on 31/05/20.
+//  Copyright © 2020 Sufiandy Elmy. All rights reserved.
+//
+
+import XCTest
+@testable import Food_with_coreml_and_coredata
+
+class Food_with_coreml_and_coredataTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
